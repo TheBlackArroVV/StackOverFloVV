17a6897f87874b4ec9a1187ba0e239980f51a8ae
TheBlackArroVV <theblackarrovv@gmail.com>
2018-04-30 12:09:39 +0300

    entering to bdd

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,18 +2,4 @@ class Answer < ApplicationRecord
   validates :body, presence: true
 
   belongs_to :question
-  belongs_to :user
-
-  scope :best, -> { order('best_answer desc') }
-
-  def make_best
-    previous_best = self.question.answers.where(best_answer: true).first
-    if previous_best
-      previous_best.best_answer = false
-      previous_best.save
-    end
-    self.best_answer = true
-    self.save
-    self.question.answers
-  end
 end
