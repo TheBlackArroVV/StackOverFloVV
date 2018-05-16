ced8993fa1325a7c9e6457c54b2266f902f436f0
TheBlackArroVV <theblackarrovv@gmail.com>
2018-05-16 18:39:08 +0300

    deploy settings

--- Gemfile
+++ Gemfile
@@ -79,6 +79,9 @@ gem 'pg'
 gem 'mysql2'
 gem 'thinking-sphinx'
 
+gem 'dotenv'
+gem 'dotenv-deployment', require: 'dotenv/deployment'
+
 group :development, :test do
   gem 'pry'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
@@ -100,6 +103,10 @@ group :development do
   gem 'letter_opener'
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'capistrano', require: false
+  gem 'capistrano-bundler', require: false
+  gem 'capistrano-rails', require: false
+  gem 'capistrano-rvm', require: false
 end
 
 group :test do
---
+++ config/deploy.rb
@@ -0,0 +1,29 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.10.2"
+
+set :application, "StackOverFlow"
+set :repo_url, "git@github.com:TheBlackArroVV/StackOverFloVV.git"
+
+# Default branch is :master
+ask :deploy, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, "/home/web/stackoverflovv"
+set :deploy_user, 'web'
+
+# Default value for :linked_files is []
+append :linked_files, "config/database.yml", ".env"
+
+# Default value for linked_dirs is []
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+
+namespace :deploy do
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :publishing, :restart
+end
